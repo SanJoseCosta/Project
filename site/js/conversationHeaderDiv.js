bf0b1f4d0f68a57973b60fedbfcb90a62d610cce
Dmitry Petrov <61933931+dmitrypetrov-dev@users.noreply.github.com>
2021-03-26 21:08:40 +0300

    Add files via upload

--- site/js/conversationHeaderDiv.js
+++ site/js/conversationHeaderDiv.js
@@ -23,11 +23,11 @@ function conversationHeaderDiv()
     :
 
     `
-        <div style="min-height: 50px;background-color: white;">
+        <div style="min-height: 50px;background-color: white; padding: 0 10px 0 0;">
             <div style="float:right;">
                 <img style="cursor: pointer; border-radius: 50%; padding: 0px;" $$click$$ src=$$local$$ height=30>
                 <br>
-                <div style="font-size:10;">
+                <div style="font-size:10; text-align: center">
                     $$localusername$$
                 </div>
             </div>
@@ -35,7 +35,7 @@ function conversationHeaderDiv()
 
         <div 
           style="display: flex;  align-items: center; color: white; background-color: #96d872; min-height: 50px; cursor: pointer;">
-            <div>
+            <div style="margin: 0 10px;">
                 <img style="border-radius: 50%;" src=$$picurl$$ height=30 width=30>
             </div>
 
