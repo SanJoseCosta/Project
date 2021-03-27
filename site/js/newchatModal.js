863bba94c783ab0eafc8966911fd92b9517ad0de
Matthew Kuenzel <matt@matthews-mbp-2.attlocal.net>
2021-03-27 09:33:20 -0400

    first commit

--- site/js/newchatModal.js
+++ site/js/newchatModal.js
@@ -5,49 +5,16 @@ function newchatModal()
     `
     	<div id="myModal" class="modal">
 
-  		  <div class="modal-content">
-  		    
-  		    <button class="close">Cancel</button>
-  		    &nbsp;
-  		    <button class="close">OK</button>
+        <div class="modal-content" style="width: 70%; background-color: white;">
+          
+          <textarea style="resize:123" placeholder=$$placeholder$$ id=txa></textarea>
+          <br>
+          <button style="padding:10px;margin:10px;" $$cancelclick$$ class="close">Cancel</button>
+          <button style="padding:10px;margin:10px;" $$okclick$$ class="close">OK</button>
+         
+        </div>
 
-  		    Username or email: <textarea style="resize:none" placeholder=$$placeholder$$></textarea>
-  		  
-  		  </div>
-
-  		</div>
-
-
-
-    <script>
-    
-      // Get the modal
-      var modal = document.getElementById("myModal");
-
-      // Get the button that opens the modal
-      var btn = document.getElementById("myBtn");
-
-      // Get the <span> element that closes the modal
-      var span = document.getElementsByClassName("close")[0];
-
-      // When the user clicks on the button, open the modal
-      btn.onclick = function() {
-        modal.style.display = "block";
-      }
-
-      // When the user clicks on <span> (x), close the modal
-      span.onclick = function() {
-        modal.style.display = "none";
-      }
-
-      // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      }
-
-    </script>
+      </div>
 
     `
 
@@ -57,7 +24,7 @@ function newchatModal()
     `
     	<div id="myModal" class="modal">
 
-  		  <div class="modal-content">
+  		  <div class="modal-content" style="width: 35%;">
   		    
   		    <textarea style="resize:none" placeholder=$$placeholder$$ id=txa></textarea>
 
@@ -67,7 +34,6 @@ function newchatModal()
   		  </div>
 
   		</div>
-
     `
 
     ;
